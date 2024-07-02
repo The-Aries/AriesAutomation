470426f07a10c6b2ec161bb83256bdd00a7d85b0
张峻豪 <111368815+The-Aries@users.noreply.github.com>
2024-07-02 19:27:16 +0800

    Update Wicresoft.meta.js

--- Wicresoft.meta.js
+++ Wicresoft.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AriesAutomation
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @updateURL    https://github.com/The-Aries/AriesAutomation/blob/main/Wicresoft.meta.js
 // @downloadURL  https://github.com/The-Aries/AriesAutomation/blob/main/Wicresoft.user.js
 // @description  该脚本用于自动刷新页面以跳过验证窗口和数学题，自动提交课程评价
@@ -11,4 +11,4 @@
 // @grant        none
 // ==/UserScript==
 // 创建日期：2024年6月30日
-// 最后更新日期：2024年6月30日
+// 最后更新日期：2024年7月2日
