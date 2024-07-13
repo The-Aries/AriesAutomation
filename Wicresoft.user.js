5536ca8dfe1c3e9dfe24af1ef9af812aa826dfb4
张峻豪 <111368815+The-Aries@users.noreply.github.com>
2024-07-13 18:57:53 +0800

    Update Wicresoft.user.js

--- Wicresoft.user.js
+++ Wicresoft.user.js
@@ -1,33 +1,44 @@
 // ==UserScript==
-// @name         AriesAutomation
-// @namespace    http://tampermonkey.net/
-// @version      0.93
-// @updateURL    https://raw.githubusercontent.com/The-Aries/AriesAutomation/main/Wicresoft.user.js
-// @downloadURL  https://raw.githubusercontent.com/The-Aries/AriesAutomation/main/Wicresoft.user.js
-// @description  该脚本用于自动刷新页面以跳过验证窗口和数学题，自动提交课程评价
-// @author       Aries
-// @match        *://v4.21tb.com/els/html/course/course.courseInfo.do?courseId=*
-// @match        *://v4.21tb.com/els/html/courseStudyItem/courseStudyItem.learn.do?courseId=*
-// @match        *://v4.21tb.com/els/html/studyCourse/studyCourse.enterCourse.do?courseId=*
-// @grant        none
+// @name AriesAutomation
+// @namespace http://tampermonkey.net/
+// @version 0.94
+// @updateURL https://raw.githubusercontent.com/The-Aries/AriesAutomation/main/Wicresoft.user.js
+// @downloadURL https://raw.githubusercontent.com/The-Aries/AriesAutomation/main/Wicresoft.user.js
+// @description 该脚本可自动课前评测，自动过视频防作弊验证，自动提交课程评价
+// @author Aries
+// @match *://v4.21tb.com/els/html/course/course.courseInfo.do?courseId=*
+// @match *://v4.21tb.com/els/html/courseStudyItem/courseStudyItem.learn.do?courseId=*
+// @match *://v4.21tb.com/els/html/studyCourse/studyCourse.enterCourse.do?courseId=*
+// @grant none
 // ==/UserScript==
 // 创建日期：2024年6月30日
-// 最后更新日期：2024年7月10日
+// 最后更新日期：2024年7月13日
 (window.onload = function ()
 {
     "use strict";
     /***********************预览页面*******************************
-     ************************预览页面******************************
-     ************************预览页面*****************************
-     ************************预览页面******************************
-     ************************预览页面*******************************/
+    ************************预览页面******************************
+    ************************预览页面*****************************
+    ************************预览页面******************************
+    ************************预览页面*******************************/
     if (window.location.href.includes("courseInfo"))
     {
         console.log("检测到预览页面，自动进入课程");
         goIntoClass();
     }
+    /***********************测试页面*******************************
+    ************************测试页面******************************
+    ************************测试页面*****************************
+    ************************测试页面******************************
+    ************************测试页面*******************************/
+    if (window.location.href.includes('studyType=STUDY'))
+    {
+        // 课前测试页面的代码
+        console.log("检测到课前测试页面，执行自动答题业务");
+        autoQuizCource();
+    }
     /***********************视频页面*******************************
-     ************************视频页面******************************
+    ************************视频页面******************************
     ************************视频页面*****************************
     ************************视频页面******************************
     ************************视频页面*******************************/
@@ -38,22 +49,28 @@
     }
 
     /***********************评价页面*******************************
-     ************************评价页面******************************
-     ************************评价页面*****************************
-     ************************评价页面******************************
-     ************************评价页面*******************************/
-    if (window.location.href.includes("studyCourse"))
+    ************************评价页面******************************
+    ************************评价页面*****************************
+    ************************评价页面******************************
+    ************************评价页面*******************************/
+    if (window.location.href.includes('willGoStep=COURSE_EVALUATE'))
     {
+        // 课程评价页面的代码
         console.log("检测到评价页面，执行自动评分业务");
         rateCourse();
     }
+    // if (window.location.href.includes("studyCourse"))
+    // {
+    //     console.log("检测到评价页面，执行自动评分业务");
+    //     rateCourse();
+    // }
 })();
 
 /***********************自定义函数*******************************
- ************************自定义函数******************************
- ************************自定义函数*****************************
- ************************自定义函数******************************
- ************************自定义函数*******************************/
+************************自定义函数******************************
+************************自定义函数*****************************
+************************自定义函数******************************
+************************自定义函数*******************************/
 // 进入课程
 function goIntoClass()
 {
@@ -175,16 +192,13 @@ function nextStep()
         const nextStepButton = document.getElementById("goNextStep");
         if (nextStepButton)
         {
-            console.log("找到'进入下一步'按钮");
+            //console.log("找到'进入下一步'按钮");
             if (!nextStepButton.classList.contains("hide"))
             {
                 console.log("按钮已显示，点击进入下一步");
                 nextStepButton.click();
                 nextButtonObserver.disconnect(); // 成功点击后停止观察
-            } else
-            {
-                console.log("按钮未显示");
-            }
+            } //else { console.log("按钮未显示"); }
         } else
         {
             console.log("未找到'进入下一步'按钮");
@@ -206,8 +220,7 @@ function nextStep()
     });
     // 限制观察范围为按钮的父元素
     const targetNode = document.getElementById("goNextStep")?.parentNode || document.body;
-    console.log("document.getElementById('goNextStep')?.parentNode-- " + document.getElementById("goNextStep")?.parentNode
-    );
+    //console.log("document.getElementById('goNextStep')?.parentNode-- " + document.getElementById("goNextStep")?.parentNode);
     // 配置观察器
     const config = {
         childList: true,
@@ -222,93 +235,35 @@ function nextStep()
 function autoPlayCourse()
 {
     nextStep();
+    var iframe = getAliPlayerFrame();
+    var iframeDocument = iframe.contentDocument;
 
-    if (getAliPlayerFrame())
+    // 监听视频元素并设置播放速度
+    setVideoPlaybackRate(iframeDocument);
+
+    if (iframe.id === "aliPlayerFrame")
     {
-        var iframe = getAliPlayerFrame();
-        // 监听视频元素并设置播放速度
-        setVideoPlaybackRate(iframe);
-
-        // 监听下一节按钮并自动点击
-        autoClickNextButton(iframe);
-    } //else { console.log("未找到aliPlayerFrame"); }
-    else
-    {
-        var old_iframe = document.getElementById("iframe_aliplayer");
-        var old_iframeDocument = old_iframe.contentDocument;
-        var old_isVideoPlaying = false; // 记录视频是否正在播放的状态
-        if (old_iframe && old_iframeDocument)
-        {
-            console.log("找到旧版aliPlayerFrame");
-            var old_videoObserver = new MutationObserver(function (mutations)
-            {
-                mutations.forEach(function (mutation)
-                {
-                    var old_video = old_iframeDocument.querySelector("video");
-                    if (mutation.type === "childList" && old_video)
-                    {
-
-                        //设置二倍速
-                        old_video.addEventListener("playing", function ()
-                        {
-                            if (old_video.playbackRate != 2.0 && old_isVideoPlaying != true)
-                            {
-                                console.log("找到视频元素");
-                                //old_video.playbackRate = 2.0;
-                                //console.log("设置视频为2倍速");
-                                old_isVideoPlaying = true; // 视频正在播放
-                            }
-                        });
-
-                        // 添加监听暂停事件的逻辑
-                        old_video.addEventListener("pause", function ()
-                        {
-                            if (old_isVideoPlaying)
-                            {
-                                // 仅当视频从播放状态转为暂停状态时刷新页面
-
-                                console.log("视频已暂停，刷新页面");
-                                location.reload();
-                            }
-                            old_isVideoPlaying = false; // 更新视频状态为暂停
-                        });
-                    }
-                    else { console.log("未找到视频元素"); }
-                });
-            });
-
-            old_videoObserver.observe(old_iframeDocument.body, {
-                childList: true,
-                subtree: true,
-            });
-        }
-        else { console.log("未找到旧版aliPlayerFrame"); };
-
-
-
-        //refreshPage();
+        // 自动播放下一章节
+        autoClickNextChapter(iframeDocument);
     }
     //refreshPage();
-
 }
 
 // 检测aliPlayerFrame
 function getAliPlayerFrame()
 {
-    console.log("寻找aliPlayerFrame");
-    var iframe = document.getElementById("aliPlayerFrame");
-    if (iframe && iframe.contentDocument)
+    // 检查两个可能的 iframe ID
+    var iframe = document.getElementById("aliPlayerFrame") || document.getElementById("iframe_aliplayer");
+    if (iframe)
     {
-        console.log("找到aliPlayerFrame");
-        console.log("aliPlayerFrame加载完毕");
+        console.log("找到 " + iframe.id);
         return iframe;
-    }
+    } else { console.log("未找到 aliPlayerFrame 或 iframe_aliplayer"); }
 }
 
 // 设置视频播放速度为2倍速
-function setVideoPlaybackRate(iframe)
+function setVideoPlaybackRate(iframeDocument)
 {
-    var iframeDocument = iframe.contentDocument;
     var isVideoPlaying = false; // 记录视频是否正在播放的状态
     var videoObserver = new MutationObserver(function (mutations)
     {
@@ -340,8 +295,7 @@ function setVideoPlaybackRate(iframe)
                     }
                     isVideoPlaying = false; // 更新视频状态为暂停
                 });
-            }
-            // else { console.log("未找到视频元素"); }
+            }// else { console.log("未找到视频元素"); }
         });
     });
 
@@ -352,11 +306,9 @@ function setVideoPlaybackRate(iframe)
 }
 
 // 监听下一节按钮并自动点击
-function autoClickNextButton(iframe)
+function autoClickNextButton(iframeDocument)
 {
-    setVideoPlaybackRate(iframe);
-
-    var iframeDocument = iframe.contentDocument;
+    setVideoPlaybackRate(iframeDocument);
     var nextButtonObserver = new MutationObserver(function (mutations)
     {
         mutations.forEach(function (mutation)
@@ -381,3 +333,90 @@ function autoClickNextButton(iframe)
     });
 }
 
+function autoClickNextChapter(iframeDocument)
+{
+    setVideoPlaybackRate(iframeDocument);
+
+    var chapterObserver = new MutationObserver(function (mutations)
+    {
+        mutations.forEach(function (mutation)
+        {
+            if (mutation.type === "attributes" && mutation.attributeName === "class")
+            {
+                autoPlayNextChapter(iframeDocument);
+            }
+        });
+    });
+
+    chapterObserver.observe(iframeDocument.body, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeFilter: ['class'],
+    });
+
+    autoPlayNextChapter(iframeDocument); // Initial call to handle cases where the chapter is already active
+}
+
+function autoPlayNextChapter(iframeDocument)
+{
+    // 获取所有章节的section-item
+    const sections = iframeDocument.querySelectorAll('.section-item');
+    // console.log('一共有' + sections.length + '个章节');
+
+    // 迭代所有章节，找到当前正在播放且已完成的章节
+    for (let i = 0; i < sections.length; i++)
+    {
+        const section = sections[i];
+        const firstLine = section.querySelector('.first-line');
+
+        if (firstLine && firstLine.classList.contains('active') && section.classList.contains('finish'))
+        {
+            console.log('找到当前播放且已完成的章节:', section);
+
+            // 查找未播放的章节
+            for (let j = 0; j < sections.length; j++)
+            {
+                const nextSection = sections[j];
+                const nextFirstLine = nextSection.querySelector('.first-line');
+
+                if (nextFirstLine && !nextFirstLine.classList.contains('active') && !nextSection.classList.contains('finish'))
+                {
+                    console.log('找到下一个未播放的章节:', nextSection);
+                    nextFirstLine.click(); // 点击未播放的章节
+                    return; // 结束函数
+                }
+            }
+        }
+    }
+}
+function autoQuizCource()
+{
+    // 选择值为5的radio按钮
+    const ratingSelector = 'input[type="radio"]';
+    const ratingButtons = document.querySelectorAll(ratingSelector);
+    ratingButtons.forEach((button) =>
+    {
+        button.click();
+        // 打印每个按钮的状态
+        console.log(
+            "Radio button value:",
+            button.value,
+            "Checked:",
+            button.checked
+        );
+    });
+
+    var submitButton = document.querySelector('.cs-submit-btn.cs-next-btn');
+    submitButton.click();
+
+    setTimeout(() =>
+    {
+        const confirmButton = document.querySelector(".layui-layer-btn1");
+        if (confirmButton)
+        {
+            confirmButton.click();
+            console.log("点击进入下一步按钮");
+        } else { console.log("未找到进入下一步按钮"); }
+    }, 500); // 延时0.5秒
+}
